3383af1ebdc5ef914cbd4913b5e5769f85f1bd5b
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-12-29 01:22:42 +0100

    test

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,22 +1,52 @@
-import { useId } from 'react'
+"use client";
 
-import { Button } from '@/components/Button'
+import { FormEvent, useRef, useState } from "react";
+import { Button } from "./Button";
 
 export function SignUpForm() {
-  let id = useId()
+  const [input, setInput] = useState("");
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log("Form submitted"); // Add this line
+
+    const email = input;
+    const button = buttonRef.current;
+
+    if (!email || !button) return;
+
+    try {
+      const res = await fetch("/api/addSubscription", {
+        body: JSON.stringify({ email }),
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+      });
+
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+
+      const data = await res.json();
+      console.log("Response data:", data); // Add this line
+    } catch (error) {
+      console.error("An error occurred while submitting the form:", error);
+    }
+  };
 
   return (
-    <form className="relative isolate mt-8 flex items-center pr-1">
-      <label htmlFor={id} className="sr-only">
+    <form className="relative isolate mt-8 flex items-center pr-1" onSubmit={handleSubmit}>
+      <label className="sr-only">
         Email address
       </label>
       <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Email address"
         required
         type="email"
-        autoComplete="email"
-        name="email"
-        id={id}
-        placeholder="Email address"
         className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
       />
       <Button type="submit" arrow>
